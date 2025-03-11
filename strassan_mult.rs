222af44d4a2133f740b92db527dbf8bfece6aa87
Sadique-The-Alchemist <sadique.pal@gmail.com>
2025-03-11 22:10:19 +0530

    with matrices

--- strassan_mult.rs
+++ strassan_mult.rs
@@ -12,3 +12,22 @@ fn standard_multiply(a: &[[i32: 2]; 2], b: &[[i32: 2]; 2]) -> [[i32; 2]; 2] {
         ]
     ]
 }
+
+// Strassan algiruthm for 2x2 matrixes
+fn strassan_2x2(a: &[[i32; 2]; 2],  b: &[[i32; 2]; 2]) -> [[i32; 2]; 2]{
+    // compute the 7 products m1 to m7 
+
+    let s1 = b[0][1] - b[1][1];
+    let s2 = a[0][0] + a[0][1];
+    let s3 = a[1][0] + a[1][1];
+    let s4 = b[1][0] - b[0][0];
+    let s5 = a[0][0] + a[1][1];
+    let s6 = b[0][0] + b[1][1];
+    let s7 = b[0][1] - b[1][1];
+    let s8 = b[1][0] + b[1][1];
+    let s9 = a[0][0] - a[1][0];
+    let s10 = b[0][0] + b[0][1];
+
+    let p1 = a[0][0] * s1 
+    let p2 = 
+}
